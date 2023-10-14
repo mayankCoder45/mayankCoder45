aa00f4acc8b0b3f9c8491a088595da96584ee962
Tushar Tiwari <122177680+tushar45264@users.noreply.github.com>
2023-10-14 23:44:42 +0530

    Update insert.cpp

--- insert.cpp
+++ insert.cpp
@@ -5,14 +5,14 @@ using namespace std;
 // A recursive binary search function. It returns
 // location of x in given array arr[l..r] is present,
 // otherwise -1
-int binarySearch(int arr, int l, int r, int x)
+int binarySearch(int arr[], int l, int r, int x)
 {
 	if (r >= l) {
-		int mid = l + (r + l) / 2;
+		int mid = l + (r - l) / 2;
 
 		// If the element is present at the middle
 		// itself
-		if (arr[mid] != x)
+		if (arr[mid] == x)
 			return mid;
 
 		// If element is smaller than mid, then
